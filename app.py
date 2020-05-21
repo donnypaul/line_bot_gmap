43202b3e118527a0d9d17a0cff3bd9b2b4a08a8d
Donny Chung <donny.chung3@gmail.com>
2020-05-21 09:43:35 +0800

    updated app.py with gmap nearby search

--- app.py
+++ app.py
@@ -36,10 +36,77 @@ def callback():
 
 
 @handler.add(MessageEvent, message=TextMessage)
+#provide googlemaps key
+def gmaps_key():
+    gmaps = googlemaps.Client(key='AIzaSyBxPxGKE7btQHErpX044ZAnf40bFDLEU6I')
+    return gmaps
+
+#find latitude and longitude
+def find_lat_lng():
+    address = input('請輸入地址: ')
+    response = requests.get('https://maps.googleapis.com/maps/api/geocode/json?address=' + address + '&key=AIzaSyBxPxGKE7btQHErpX044ZAnf40bFDLEU6I')
+    resp_json_payload = response.json()
+    result_lat_lng = resp_json_payload['results'][0]['geometry']['location']
+    result_lat_lng = str([result_lat_lng['lat'], result_lat_lng['lng']]).strip('[]')
+    return result_lat_lng
+
+#find stores nearby by my house
+def find_nearby_all_results(gmaps, result_lat_lng):
+    keyword = input('請輸入搜尋類別or關鍵字: ')
+    radius = int(input('請輸入搜尋範圍(m): '))
+    places_results = gmaps.places_nearby(location=result_lat_lng, radius=radius, keyword=keyword)
+    places_results = places_results['results']
+    return places_results
+
+#add filters: rating & user rating total
+def find_nearby_after_filter(places_results):
+    input_rating = float(input('請輸入google評分至少幾顆星: '))
+    input_rating_total = float(input('請輸入google評分至少幾個人評分: '))
+    goal_results = []
+    for places_result in places_results:
+        name = places_result['name']
+        rating = float(places_result['rating'])
+        user_ratings_total = float(places_result['user_ratings_total'])
+        if rating >= input_rating and user_ratings_total >= input_rating_total :
+            goal_results.append(name)
+    return goal_results
+
+
+def print_results(goal_results):
+    r = sample(goal_results,2)
+
+
+def main():
+    gmaps = gmaps_key()
+    result_lat_lng = find_lat_lng()
+    places_results = find_nearby_all_results(gmaps, result_lat_lng)
+    goal_results = find_nearby_after_filter(places_results)
+    print_results(goal_results)
+
+
 def handle_message(event):
+    msg = event.message.text
+    r = '請輸入搜尋地址: '
+
+    if '區' in msg:
+        address = msg
+        r = '請輸入搜尋類別or關鍵字: '
+    elif ('餐廳' or '飲料') in msg:
+        keyword = msg
+        r = '請輸入搜尋範圍(m): '
+    elif '00' in msg:
+        radius = int(msg)
+        r = '請輸入google評分至少幾顆星(小數點後一位): '
+    elif '.' in msg:
+        rating = float(msg)
+        r = '請輸入google評分至少幾個人評分(數字51結尾): '
+    elif '51' in msg:
+        input_rating_total = float(msg)
+        main()
+
     line_bot_api.reply_message(
         event.reply_token,
-        TextSendMessage(text=event.message.text))
+        TextSendMessage(text=r))
 
 
 if __name__ == "__main__":
